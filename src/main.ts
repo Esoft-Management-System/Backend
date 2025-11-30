c8d05c659370454c1431779e4867fb548cf1965c
Harishanth03 <kandeepanharishanth@gmail.com>
2025-11-30 09:00:35 +0530

    feat: add Swagger documentation setup and dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Esoft Management API')
+    .setDescription('API documentation')
+    .setVersion('1.0')
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+      'access-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(`${globalPrefix}/docs`, app, document);
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   console.log(`Server running on http://localhost:${port}`);
+  console.log(
+    'Swagger docs available on: ',
+    `http://localhost:${port}/${globalPrefix}/docs`,
+  );
 }
-bootstrap();
+
+void bootstrap();
