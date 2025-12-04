d597ffd33d3ffce5d7201e4b1b1914c1e3a027a7
Ruvethika <ruvethiruve74@gmail.com>
2025-12-04 18:30:35 +0530

    i have done StudentRegistration

--- src/student-reg/dto/Schemas/student-reg.schema.ts
+++ src/student-reg/dto/Schemas/student-reg.schema.ts
@@ -1,34 +1,33 @@
-import { Prop, Schema,SchemaFactory} from "@nestjs/mongoose";
-import { Document } from "mongoose";
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
 
+export type StudentRegistrationDocument = StudentRegistration & Document;
 
-export type StudentRegDocument = StudentReg & Document;
-
-@Schema({timestamps: true})
-export class StudentReg {
-  @Prop({required: true, unique: true})
+@Schema({ timestamps: true })
+export class StudentRegistration {
+  @Prop({ required: true, unique: true })
   eNumber: string;
 
-  @Prop({required: true})
+  @Prop({ required: true })
   fullName: string;
 
-  @Prop({required: true})
+  @Prop({ required: true })
   contactNumber: string;
 
-  @Prop({required: true}) 
+  @Prop({ required: true })
   dateOfBirth: Date;
 
-  @Prop({required: true})
+  @Prop({ required: true })
   nic: string;
 
-  @Prop({required: true, unique: true})
+  @Prop({ required: true, unique: true })
   emailAddress: string;
 
-  @Prop({required: true})
+  @Prop({ required: true })
   address: string;
 
-  @Prop({required: true})
+  @Prop({ required: true })
   password: string;
-
 }
-export const StudentRegSchema = SchemaFactory.createForClass(StudentReg);
+export const StudentRegistrationSchema =
+  SchemaFactory.createForClass(StudentRegistration);
