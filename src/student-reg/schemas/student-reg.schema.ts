7deee1646fced9406934c6d8fae15437a0562989
OrinDev02 <orinlavaniya04@gmail.com>
2025-12-04 11:44:46 +0530

    feat(logic): I have done student-reg.schema.ts file

---
+++ src/student-reg/schemas/student-reg.schema.ts
@@ -0,0 +1,33 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+export type StudentRegDocument = StudentReg & Document;
+
+@Schema({ timestamps: true })
+export class StudentReg {
+  @Prop({ required: true })
+  eNumber: string;
+
+  @Prop({ required: true })
+  fullName: string;
+
+  @Prop({ required: true })
+  contactNumber: string;
+
+  @Prop({ required: true })
+  dateOfBirth: Date;
+
+  @Prop({ required: true })
+  nic: string;
+
+  @Prop({ required: true, unique: true })
+  emailAddress: string;
+
+  @Prop({ required: true })
+  address: string;
+
+  @Prop({ required: true })
+  password: string;
+
+}
+
+export const StudentRegSchema = SchemaFactory.createForClass(StudentReg);
