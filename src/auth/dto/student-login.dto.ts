c790ba0715143d700be78b543a14d3a1fd23c4e1
Harishanth03 <kandeepanharishanth@gmail.com>
2025-12-09 11:44:35 +0530

    feat(dto): i have done dto stracture for the Student login

---
+++ src/auth/dto/student-login.dto.ts
@@ -0,0 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class StudentLoginDto {
+  // E-Number
+  @ApiProperty({
+    type: String,
+    example: '',
+    description: 'Please enter your student E-Number',
+  })
+  @IsNotEmpty({
+    message: 'E-Number is required',
+  })
+  @IsString()
+  eNumber: string;
+
+  // Password
+  @ApiProperty({
+    type: String,
+    example: '',
+    description: 'Please enter your password',
+  })
+  @IsNotEmpty({
+    message: 'Password is required',
+  })
+  @IsString()
+  password: string;
+}
