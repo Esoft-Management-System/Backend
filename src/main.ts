c5bdb9e9651d4922671fa1123a8de508b035b066
Harishanth03 <kandeepanharishanth@gmail.com>
2026-01-05 11:58:23 +0530

    Refactor start command in package.json and update render.yaml for deployment configuration

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { logger: false });
   const server = app.getHttpAdapter().getInstance();
   server.get('/', (req, res) => res.send('Server is running'));
 
